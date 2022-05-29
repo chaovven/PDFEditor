89e705e904ff05b5260761f70bf55282a6065e17
chaowen <chaowen@nuaa.edu.cn>
2022-05-29 17:08:56 +0800

    finish pdf ocr

--- upload.js
+++ upload.js
@@ -1,31 +1,85 @@
-var http = require('http');
-const multer = require('multer');
-var fs = require('fs');
+var express = require('express');
+var app = express();
+var fs = require("fs");
+const { exec } = require('child_process');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(multer({ dest: __dirname + '/uploads/' }).any());
+
+// Get root
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + "/" + "index.html");
+})
 
 
-var server = http.createServer();
+// 
+app.post('/pdf-ocr', function (req, res) {
+  cmd = ""
 
-server.on('request', function (req, res) {
-  // process the req here
-  if (req.url == '/fileupload') {
-    var form = new formidable.IncomingForm();
-    form.multiples = true; //use this while dealing with multiple files
-    console.log("HEllo")
-    form.parse(req, function (err, fields, files) {
-      var oldpath = files.uploadfile.filepath;
-      var newpath = './' + files.uploadfile.originalFilename;
-      fs.rename(oldpath, newpath, function (err) {
-        if (err) throw err;
-        res.write('File Uploaded!')
-        return res.end();
-      });
-    })
-  } else {
-    fs.readFile('index.html', function (err, data) {
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.write(data);
-      return res.end();
-    });
+  // construct cmd
+  for (var i = 0; i < req.files.length; i++) {
+    let filepath = req.files[i].path,
+      newFilename = __dirname + '/' + req.files[i].originalname
+    cmd = cmd + `ocrmypdf -l chi_sim "${filepath}" "${newFilename}"\n`
   }
+
+  // compress if length > 1
+  if (req.files.length >= 2) {
+    let filelist = ""
+    req.files.forEach(file => {
+      filelist += `"${file.originalname}" `
+    })
+    cmd += `tar -cvf "./uploads/ocr_${req.files.length}files.zip" -C ${__dirname} ${filelist}`
+  }
+
+  console.log("\nTry to execute the following cmd: ")
+  console.log(cmd)
+
+  // execute cmd
+  exec(cmd, (err, stdout, stderr) => {
+    console.log('\n' + cmd)
+    if (err) {
+      console.log(stderr)
+    } else {
+      // delete files after finishing executing cmd
+      console.log(stderr)
+
+      // download filename
+      download_fn = req.files.length >= 2 ? `./uploads/ocr_${req.files.length}files.zip` : __dirname + '/' + req.files[0].originalname
+
+      // download
+      res.download(download_fn, (d_err) => {
+        if (d_err) {
+          console.log(d_err)
+        }
+        // delete files
+        console.log("\nTrying to deleting files")
+        for (var i = 0; i < req.files.length; i++) {
+          fs.unlinkSync(req.files[i].path)
+          console.log(`\n${req.files[i].path} deleted!`)
+          fs.unlinkSync(__dirname + '/' + req.files[i].originalname)
+          console.log(`\n${req.files[i].originalname} deleted!`)
+        }
+        if (req.files.length >= 2) {
+          fs.unlinkSync(`./uploads/ocr_${req.files.length}files.zip`)
+          console.log(`./uploads/ocr_${req.files.length}files.zip deleted`)
+        }
+      })
+    }
+  })
 });
-server.listen(8080);
+
+app.post('/pdf-merge', function (req, res) {
+  req.files.forEach(e => {
+  })
+});
+
+var server = app.listen(8081, function () {
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log(`Example app listening at http://${host}:${port}`)
+})
